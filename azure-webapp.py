48264eb60058428bdb3deddf647ec2b323c29154
Kaushik <kaushik.asp@rentomojo.com>
2018-01-13 17:59:33 +0530

    modified db code

--- azure-webapp.py
+++ azure-webapp.py
@@ -4,6 +4,8 @@ import pypyodbc as pyodbc
 connection_string = "Driver={ODBC Driver 13 for SQL Server};Server=tcp:sqldbplatingnum.database.windows.net,1433;Database=cozydb;Uid=platingnum@sqldbplatingnum;Pwd={D0wnl0ad!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 app = Flask(__name__)
 
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	if request.method == 'POST':
@@ -12,6 +14,7 @@ def home():
 		query_string = "INSERT TO CampaingTracker(CampaignName,CampaignDetails,CampaignStartDate,CampaignEndDate,Campaign_ID) VALUES({},{},{},{},{})".format(data['campaign_name'],data['campaign_details'],data['campaign_startDate'],data['campaign_endDate'],data['campaign_uniqueId'])
 		# Logic to store the data in the database
 		try:
+			db = pyodbc.connect(connection_string)
 			db.cursor().execute(query_string)
 			resp_dict = { 'success': 'true' }			
 		except Exception as e:
