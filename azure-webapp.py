dfa7cfc684e2c8cb67e63d07f970b3890c5bb014
Kaushik <kaushik.asp@rentomojo.com>
2018-01-25 01:20:13 +0530

    reverted to default db

--- azure-webapp.py
+++ azure-webapp.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, request, jsonify
-import pymysql
-server='sqldbplatingnum.database.windows.net'
-user="platingnum@sqldbplatingnum"
-password="D0wnl0ad!"
-db = pymysql.connect(server, user, password, db="cozydb", port=1433)
-# connection_string = "Driver={ODBC Driver 13 for SQL Server};Server=tcp:sqldbplatingnum.database.windows.net,1433;Database=cozydb;Uid=platingnum@sqldbplatingnum;Pwd={D0wnl0ad!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+import pypyodbc as pyodbc
+
+connection_string = "Driver={ODBC Driver 13 for SQL Server};Server=tcp:sqldbplatingnum.database.windows.net,1433;Database=cozydb;Uid=platingnum@sqldbplatingnum;Pwd={D0wnl0ad!};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 app = Flask(__name__)
 
 
@@ -24,13 +21,15 @@ def home():
 			return jsonify(resp_dict)
 		# Logic to store the data in the database
 		try:
+
+			db = pyodbc.connect(connection_string)
 			db.cursor().execute(query_string)
 			db.commit()
-			resp_dict = { 'success': 'true', 'statusCode': '200', 'uniqueId': data['campaign_uniqueId']}			
 			db.close()
-			return jsonify(resp_dict)
 		except Exception as e:
 			resp_dict = { 'error': str(e), 'statusCode': '400' }
+		resp_dict = { 'success': 'true', 'statusCode': '200', 'uniqueId': data['campaign_uniqueId']}			
+		return jsonify(resp_dict)
 
 
 	elif request.method == 'GET':
@@ -42,8 +41,9 @@ def home():
 def searchData():
 	try:
 		query_string = "SELECT * FROM CampaingTracker"
+		db = pyodbc.connect(connection_string)
 		db.cursor().execute(query_string)
-		query_results = [dict(zip([column[0] for column in db.cursor().description], row)) for row in db.cursor().fetchall()]
+		query_results = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
 		resp_dict = { 'success': 'true', 'statusCode': '200', 'data': query_results}			
 		return jsonify(query_results)
 	except Exception as e:
